75d29e6144e63b0f18826d593dddbec7d1dda579
peter michael <mikedami001@gmail.com>
2023-02-13 10:22:14 +0100

    update
    quick alx knowledge to test

--- quiz.py
+++ quiz.py
@@ -9,7 +9,7 @@ print("All answers in lowercase please ")
 print("Answer yes or no in the questions")
 print("\n" * 3)
 
-playing = input("Do you wish to play? \n")
+playing.lower() = input("Do you wish to play? \n")
 print("type yes to proceed")
 if playing != "yes":
     quit()
@@ -19,35 +19,35 @@ print('*' * 10)
 print("Start")
 print('*' * 10)
 
-answer = input("Is the ALX Certificate accepted globlally?\n")
+answer.lower() = input("Is the ALX Certificate accepted globlally?\n")
 if answer == "yes":
     print("correct")
 else:
     print("incorrect")
 print("ALX and Holberton certificate is an internationally recognized coding school")
 
-answer = input("what does ALX stand for?\n")
+answer.lower() = input("what does ALX stand for?\n")
 if answer == "Africa Leadership Academy".lower():
     print("correct")
 else:
     print("incorrect")
 print("ALX Stand for africa leadership but the 'X' Is to be interpreted by you")
 
-answer = input("who is the founder of ALX Group ?\n")
+answer.lower() = input("who is the founder of ALX Group ?\n")
 if answer == "fred swaniker".lower():
     print("correct")
 else:
     print("incorrect")
 print("fred swaniker established alx in 2018")
 
-answer = input("Are ALX courses self paced?\n")
+answer.lower = input("Are ALX courses self paced?\n")
 if answer == "yes":
     print("correct")
 else:
     print("incorrect")
 print("alx courses are online and self paced")
 
-answer = input("Who funds ALX?\n")
+answer.lower() = input("Who funds ALX?\n")
 if answer == "MASTERCARD FOUNDATION".lower():
     print("correct")
 else:
